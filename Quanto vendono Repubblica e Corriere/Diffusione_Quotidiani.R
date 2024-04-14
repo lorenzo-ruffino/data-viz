735705ab15c38d3157ac06493ee3444706d3d2b6
Lorenzo Ruffino <github@mail.lorenzoruffino.com>
2024-04-14 10:41:14 +0200

    Add files via upload

---
+++ Quanto vendono Repubblica e Corriere/Diffusione_Quotidiani.R
@@ -0,0 +1,130 @@
+library(tidyverse)
+library(readxl)
+library(showtext)
+library(lubridate)
+library(zoo)
+
+# Scarica tutti i file dal 2012 al 2024
+
+mesi = seq(1, 12, by=1)
+anni = seq(2012, 2024, by=1)
+
+
+if (!file.exists("downloaded_files")) {
+  dir.create("downloaded_files")
+}
+
+
+for (anno in anni) {
+  for (mese in mesi) {
+    
+    link <- paste0("https://www.adsnotizie.it/Dati/downloadDMS?mese=", mese, "&anno=", anno, "&periodicita=1&nomeReport=DMSFilePubblicato_3")
+    
+    nome_file <- paste0("DMS_", anno, "_", sprintf("%02d", mese), ".xls")
+    
+    percorso_file <- file.path("downloaded_files", nome_file)
+    
+    download.file(link, destfile = percorso_file, mode = "wb")
+    
+    Sys.sleep(2)
+  }
+}
+
+
+# Leggi tutti i file
+
+df = data.frame()
+
+for (anno in anni) {
+  for (mese in mesi) {
+
+    nome_file = paste0("DMS_", anno, "_", sprintf("%02d", mese), ".xls")
+    
+    percorso_file = file.path("downloaded_files", nome_file)
+    
+    dati = read_excel(percorso_file)
+    
+    df = rbind(df, dati)
+  }
+}
+
+
+# Filtra i dati
+  
+data = df%>%
+  filter(...1 %in% c("CORRIERE DELLA SERA", "REPUBBLICA (LA)"))%>%
+  rename(testata = ...1,
+         anno = ...6,
+         mese = ...7)%>%
+  mutate(diffusione = as.numeric(`Diffusione media Italia cartacea e digitale`)+as.numeric(...12)+as.numeric(...13)+as.numeric(...14))%>%
+  select(testata, anno, mese, diffusione)%>%
+  mutate(testata = ifelse(testata == "CORRIERE DELLA SERA", "Corriere", "Repubblica"),
+         diffusione = as.numeric(diffusione),
+         date = as.Date(paste(anno, mese, "01", sep = "-"), format = "%Y-%m-%d"))%>%
+  group_by(testata)%>%
+  mutate(media = rollmean(diffusione, k = 6, align = "right", fill = NA))
+
+
+
+
+# Tema e grafico
+
+font_add_google("Source Sans Pro")
+showtext_auto()
+
+theme_linechart <- function(...) {
+  theme_minimal() +
+    theme(
+      text=element_text(family="Source Sans Pro"),
+      legend.position="right",
+      axis.line = element_line(linewidth = 1),
+      axis.text = element_text(size = 50,  color = "#1C1C1C", hjust = 0.5,  margin = margin(b = 0, t = 0, l = 0, unit = "cm")),
+      axis.title = element_text(size = 50,  color = "#1C1C1C", hjust = 0.5,  margin = margin(b = 0, t = 0, l = 0.5, unit = "cm")),
+      axis.ticks = element_blank(),
+      panel.background = element_blank() ,
+      panel.grid.major = element_blank(), 
+      panel.grid.minor = element_blank(), 
+      plot.background = element_blank(),
+      legend.background = element_blank(),
+      legend.box.background = element_blank(),
+      legend.key = element_blank(),
+      panel.border = element_blank(),
+      legend.title=element_blank(),
+      plot.margin = unit(c(b = 0.8, l = 0.8, t = 0.8, r=0.8), "cm"),
+      plot.title.position = "plot",
+      legend.text =    element_text(size = 50,  color = "#1C1C1C", hjust = 0, margin = margin(b = 0, l = 0, t = 0, r=0, unit = "cm")),
+      plot.title =     element_text(size = 90,  color = "#1C1C1C", hjust = 0, margin = margin(b = 0, l = 0, t = 0, r=0, unit = "cm")),
+      plot.subtitle =  element_text(size = 55,  color = "#1C1C1C", hjust = 0, margin = margin(b = 1, l = 0, t = 0.1, r=0, unit = "cm")),
+      plot.caption =   element_text(size = 50,  color = "#1C1C1C", hjust = 1, margin = margin(b = 0, l = 0, t = 1, r=0, unit = "cm")),
+      ...
+    )
+}
+
+
+png("Diffusione_repubblica_corriere.png", width = 9.5, height = 10, units="in", res=300)
+ggplot(data, aes(x=date, y=media, group=testata, colour=testata)) +
+  theme_linechart()+
+  geom_line(linewidth = 1.5, show.legend = F)+
+  scale_color_manual(values =c("Repubblica"="#F12938", "Corriere"="#0478EA"))+
+  scale_fill_manual(values =c("Repubblica"="#F12938", "Corriere"="#0478EA"))+
+  geom_text(data%>%filter(date == max(date)),
+            mapping= aes(x=date, y=media-15000, group=testata, colour=testata, label=testata), size=20, hjust=1, fontface="bold", show.legend = FALSE, family="Source Sans Pro")+
+  scale_y_continuous(limits=c(0,400000), breaks=seq(0, 500000, by=100000), labels = function(x) paste0(x/1000, " mila"), expand = c(0.01, 0))+
+  scale_x_date(limits = c(as.Date("2014-01-01"), as.Date("2024-3-01")),
+               date_breaks = "1 year",
+               date_labels = "%Y",
+               expand = c(0.01, 0))+
+  labs(x = NULL,
+       y = NULL,
+       title = "Quanto vendono Repubblica e Corriere", 
+       subtitle = "Media mobile a sei mesi del totale delle vendite individuali di Repubblica e Corriere",  
+       caption = paste("Elaborazione di Lorenzo Ruffino | Fonte dati: Accertamenti Diffusione Stampa"))
+dev.off()
+
+
+data %>%
+  filter(date %in% c(as.Date("2014-02-01"), as.Date("2024-02-01")))%>%
+  group_by(testata)%>%
+  mutate(variazione = (media / lag(media, 1)) -1 )
+
+write.csv(data%>%select(testata, date, diffusione, media), file="Diffusione_repubblica_corriere.csv")
